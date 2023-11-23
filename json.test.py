23d5f55351bd22147f087931db2b03e90cc40dd6
rsancho64 <rsancho@iesrioarba.es>
2023-11-23 07:38:43 +0100

    .

--- json.test.py
+++ json.test.py
@@ -1,4 +1,23 @@
-# STUDIO: https://www.w3schools.com/python/python_json.asp
+# STUDIO:
+# 
+# https://www.w3schools.com/python/python_json.asp
+
+# - [ ] try a complex json like this ...
+# 1	{
+# 2	    "firstName": "John",
+# 3	    "lastName": "Smith",
+# 4	    "address": {
+# 5	        "streetAddress": "21 2nd Street",
+# 6	        "city": "New York",
+# 7	        "state": "NY",
+# 8	        "postalCode": 10021
+# 9	    },
+# 10	    "phoneNumbers": [
+# 11	        "212-732-1234",
+# 12	        "646-123-4567"
+# 13	    ]
+# 14	}
+# ... from https://www.ibm.com/docs/es/baw/20.x?topic=formats-javascript-object-notation-json-format
 
 import json
 
@@ -31,3 +50,4 @@ ss_js = json.loads(ss)
 
 # the result is a Python dictionary:
 # print(js["age"])
+
